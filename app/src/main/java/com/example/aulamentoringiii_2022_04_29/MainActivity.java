d19b709bf88ae0007a93428ed262b799c90e326e
udofritzke <udo.fritzke@gmail.com>
2022-04-29 20:24:47 -0300

    introdução de um botao para atualizaçao da mensagem

--- app/src/main/java/com/example/aulamentoringiii_2022_04_29/MainActivity.java
+++ app/src/main/java/com/example/aulamentoringiii_2022_04_29/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.aulamentoringiii_2022_04_29;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("aplicacao", "executou onCreate()");
+
+    }
+    public void trataBotao(View b){
+        texto = (TextView) findViewById(R.id.texto);
+        texto.setText("Ola todo mundo");
     }
 }
